28833df508f91f912000ab17909347d151151680
ivan <ivan.s.petrov37@gmail.com>
2026-08-14 11:52:38 +0200

    loading documents from csv

---
+++ document_loader.py
@@ -0,0 +1,35 @@
+from langchain_core.documents import Document
+import pandas as pd
+from pathlib import Path
+
+ID = "id"
+BOOK_TITLE = "book_title"
+ENTRY = "entry"
+
+def csv_to_documents(path: Path) -> list[Document]:
+
+    if not path.is_file():
+        raise FileNotFoundError(f"The CSV file not found: {path}")
+
+    documents = []
+
+    df = pd.read_csv(path)
+
+    for index, row in df.iterrows():
+        content = (
+            f"title: {row[BOOK_TITLE]}\n"
+            f"entry: {row[ENTRY]}"
+        )
+
+        documents.append(
+            Document(
+                page_content=content,
+                metadata={
+                    "source": str(path),
+                    "row": index,
+                    "id": row[ID]
+                }
+            )
+        )
+
+    return documents
